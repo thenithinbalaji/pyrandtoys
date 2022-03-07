d32ff1a6723643da9af98c1f6350aa20f15dfcfa
Dhravya <dhravyashah@gmail.com>
2022-03-07 23:40:00 +0530

    Added docstrings, Typehinting and isinstance for checking the type

--- pyrandtoys/functions.py
+++ pyrandtoys/functions.py
@@ -1,7 +1,19 @@
 import random as rop
+from typing import Tuple
 
 
-def dice(count=1):
+def dice(count: int = 1) -> Tuple[int, ...]:
+    """
+    Rolls N dices and returns the values as a tuple
+
+    Parameters:
+    count (int) : The number of dices to roll
+
+    Returns:
+    tuple: The values of dice rolls
+    The tuple values is a number between 1 and 6
+   """
+
     res = ()
 
     for _ in range(int(count)):
@@ -10,7 +22,18 @@ def dice(count=1):
     return res
 
 
-def coin(count=1):
+def coin(count: int = 1) -> Tuple[str, ...]:
+    """
+    Rolls N coins and returns the values as a tuple
+
+    Parameters:
+    count (int) : The number of coins to roll
+
+    Returns:
+    tuple: The values of coin rolls
+    tuple can be HEADS or TAILS
+   """
+
     res = ()
     sample_space = ["HEADS", "TAILS"]
 
@@ -20,7 +43,18 @@ def coin(count=1):
     return res
 
 
-def dreidel(count=1):
+def dreidel(count: int = 1) -> Tuple[str, ...]:
+    """
+    Spin a dreidel N times and return the values as a tuple
+
+    Parameters:
+    count (int) : The number times to spin
+
+    Returns:
+    tuple: The values of dreidel spins
+    Tuple contains one of the values between ["NUN", "GIMEL", "HE", "SHIN"]
+   """
+
     res = ()
     sample_space = ["NUN", "GIMEL", "HE", "SHIN"]
 
@@ -30,7 +64,17 @@ def dreidel(count=1):
     return res
 
 
-def cat(count=1):
+def cat(count: int = 1) -> Tuple[str, ...]:
+    """
+    Ask if the cat is alive or dead
+
+    Parameters:
+    count (int) : The number of times to ask
+
+    Returns:
+    tuple: The dead or alive values of the cats
+    Cat can be ALIVE or DEAD
+   """
     res = ()
     sample_space = ["ALIVE", "DEAD"]
 
@@ -40,7 +84,17 @@ def cat(count=1):
     return res
 
 
-def switch(count=1):
+def switch(count: int = 1) -> Tuple[str, ...]:
+    """
+    Flip a switch N times and return the values as a tuple
+
+    Parameters:
+    count (int) : The number times to spin
+
+    Returns:
+    tuple: The values of dreidel spins
+    Switch can be ON or OFF
+   """
     res = ()
     sample_space = ["ON", "OFF"]
 
@@ -50,22 +104,42 @@ def switch(count=1):
     return res
 
 
-def spinner(lower, upper=0):
+def spinner(lower: int, upper: int = 0) -> int:
+    """
+    Spin a spinner between a lower and upper value
+
+    Parameters:
+    lower (int) : The lower value of the spinner
+    upper (int) : The upper value of the spinner
+
+    Returns:
+    int: The value of the spinner
+    int is a number between lower and upper
+    """
     if lower > upper:
         lower, upper = upper, lower
 
     return rop.randint(int(lower), int(upper))
 
 
-def combi(list=[]):
-    res = ()
+def combi(list: list = []) -> Tuple[str, ...]:
+    """
+    Randomise combinations between other functions
 
-    if str(type(list)) == "<class 'str'>":
+    Parameters:
+    list (list) : The list of functions to randomise
+
+    Returns:
+    tuple: The values of the randomised functions
+    """
+    res = ()
+    
+    if isinstance(list, list):
         temp = []
         temp.append(list)
         list = temp
 
-    elif str(type(list)) == "<class 'int'>":
+    elif isinstance(list, int):
         temp = []
         sample_space = ["coin", "dice", "switch"]
 
